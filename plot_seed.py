9ca62ba4b37559fce4e4d4c5b4fb120d41853ce9
nao008 <naoya-0829@i.softbank.jp>
2023-11-17 10:28:13 +0900

    [upgrade plot_seed.py] plot all data

--- plot_seed.py
+++ plot_seed.py
@@ -21,6 +21,7 @@ args = parser.parse_args()
 dataname = args.dataname
 alldata = args.data
 log = {}
+
 seed_vals = [42, 666, 2023, 1, 3]
 list_len = len(seed_vals)
 # ファイル名のリスト
@@ -33,37 +34,34 @@ df_list = [pd.read_csv(filename, sep=" ", header=None) for filename in filenames
 labels = [f"seed:{seed_vals}" for seed_vals in seed_vals]
 colors = ['red', 'blue', 'green', 'yellow', 'purple']
 
-# プロットしたい値のリスト
-values = [0, 1, 2, 3, 4, 5, 6]
 # 抽出した全要素をまとめるdf
-all_nearest_df = pd.DataFrame()
-for value in values:
-    #valueごとに抽出した要素をまとめるdf
-    nearest_df = pd.DataFrame()
+all_df = pd.DataFrame()
+for idx in range(len(df_list[0])):
+    middle_df = pd.DataFrame()
     for df in df_list:
-        # カラム0の値がvalueに最も近い行を見つける
-        nearest = df.iloc[(df[0]-value).abs().argsort()[:1]]
-        nearest_df = pd.concat([nearest_df, nearest]).reset_index(drop=True)
-    # print(nearest_df)
+        middle_df = pd.concat([middle_df, df.iloc[idx]], axis=1)
+    #middle_dfのindexとcolumnsを入れ替え
+    middle_df = middle_df.T.reset_index(drop=True)
+    # print(middle_df)
+    #middle_dfをall_dfに追加
+    all_df = pd.concat([all_df, middle_df], axis=0)
     #logに残す
-    max_value = nearest_df[1].max()
-    min_value = nearest_df[1].min()
-    max_index = nearest_df[1].idxmax()
-    min_index = nearest_df[1].idxmin()
-    log[f"{nearest_df[0][0]}_max:{seed_vals[max_index]}"] = max_value
-    log[f"{nearest_df[0][0]}_min:{seed_vals[min_index]}"] = min_value
-    all_nearest_df = pd.concat([all_nearest_df, nearest_df]).reset_index(drop=True)
-
-
-print(log)
+    max_value = middle_df[1].max()
+    min_value = middle_df[1].min()
+    max_index = middle_df[1].idxmax()
+    min_index = middle_df[1].idxmin()
+    log[f"{middle_df[0][0]}_max:{seed_vals[max_index]}"] = max_value
+    log[f"{middle_df[0][0]}_min:{seed_vals[min_index]}"] = min_value
+#all_dfのindexをリセット
+all_df = all_df.reset_index(drop=True)
 
 # 散布図のplot
 fig1, ax1 = plt.subplots()
 # 凡例用のハンドラとラベルを保存するリスト
 handlers = []
 labels_list = []
-for i in range(len(all_nearest_df)):
-    scatter = ax1.scatter(all_nearest_df[0][i], all_nearest_df[1][i], color=colors[i%len(labels)], label=labels[i%len(labels)])
+for i in range(len(all_df)):
+    scatter = ax1.scatter(all_df[0][i], all_df[1][i], color=colors[i%len(labels)], label=labels[i%len(labels)])
     # 同じラベルが既にリストに存在しない場合に、ハンドラとラベルを追加
     if labels[i%len(labels)] not in labels_list:
         handlers.append(scatter)
@@ -80,10 +78,10 @@ plt.close()
 #エラーバーのplot
 fig2, ax2 = plt.subplots()
 list = []
-for i in range(len(all_nearest_df)):
-    list.append(all_nearest_df[1][i])
+for i in range(len(all_df)):
+    list.append(all_df[1][i])
     if i % len(labels) == len(labels)-1:
-        plt.vlines(all_nearest_df[0][i], min(list), max(list), color='black')
+        plt.vlines(all_df[0][i], min(list), max(list), color='black')
         list = []
 # 軸の名前を設定
 plt.xlabel('true(eV)')
